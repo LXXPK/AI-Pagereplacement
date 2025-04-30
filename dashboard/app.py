75430b4c7498f807f16d7d003431373b27236895
B_Kiran <124519398+LXXPK@users.noreply.github.com>
2025-04-30 08:56:23 +0530

    all three traditional alorithms are added

--- dashboard/app.py
+++ dashboard/app.py
@@ -48,12 +48,11 @@
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# FIFO Algorithm (Already Implemented)
+# FIFO Algorithm
 def fifo_page_replacement(pages, frame_size):
     frames = []
     matrix = []
@@ -78,10 +77,10 @@ def fifo_page_replacement(pages, frame_size):
                 column.append("")
         matrix.append(column)
         status_list.append(status)
-    
+
     return matrix, status_list, page_faults
 
-# LRU Algorithm Implementation
+# LRU Algorithm
 def lru_page_replacement(pages, frame_size):
     frames = []
     matrix = []
@@ -118,6 +117,56 @@ def lru_page_replacement(pages, frame_size):
 
     return matrix, status_list, page_faults
 
+# Optimal Algorithm
+def optimal_page_replacement(pages, frame_size):
+    frames = []
+    matrix = []
+    status_list = []
+    page_faults = 0
+
+    for i, page in enumerate(pages):
+        status = "MISS"
+
+        # If the page is already in memory, it's a hit
+        if page in frames:
+            status = "HIT"
+        else:
+            # Page fault occurred
+            status = "MISS"
+            page_faults += 1
+
+            if len(frames) < frame_size:
+                frames.append(page)  # Add the page if there is space
+            else:
+                # Find the optimal page to remove
+                farthest_index = -1
+                page_to_remove = None
+
+                for f in frames:
+                    if f not in pages[i:]:
+                        page_to_remove = f
+                        break
+                    else:
+                        next_use = pages[i:].index(f)  # find the next usage of f
+                        if next_use > farthest_index:
+                            farthest_index = next_use
+                            page_to_remove = f
+
+                frames.remove(page_to_remove)
+                frames.append(page)
+
+        # Build matrix column-wise for visualization
+        column = []
+        for i in range(frame_size):
+            if i < len(frames):
+                column.append(frames[i])
+            else:
+                column.append("")
+        matrix.append(column)
+        status_list.append(status)
+
+    return matrix, status_list, page_faults
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -127,6 +176,8 @@ def index():
 
         if algorithm == "LRU":
             matrix, status_list, page_faults = lru_page_replacement(pages, frame_size)
+        elif algorithm == "Optimal":
+            matrix, status_list, page_faults = optimal_page_replacement(pages, frame_size)
         else:  # Default to FIFO
             matrix, status_list, page_faults = fifo_page_replacement(pages, frame_size)
 
